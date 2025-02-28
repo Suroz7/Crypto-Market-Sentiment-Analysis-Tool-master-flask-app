22ff7ca4d4fea32b8d508c0c410b6822a745dbfc
Suroz7 <48718555+Suroz7@users.noreply.github.com>
2025-02-28 12:10:19 +0200

    added config
    added config

---
+++ config.py
@@ -0,0 +1,6 @@
+# Configuration file for API keys and other sensitive information
+import os
+# CoinMarketCap API Key
+COINMARKETCAP_API_KEY = "88e17407-d0c9-4b62-9e9b-ff4a213291f3"
+
+OLLAMA_HOST = 'http://35.228.220.71:11434'
